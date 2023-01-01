d12ab6dd6ad7b82ebcc959e37a7206d8e11cf2c9
skyclouds2001 <skyclouds2001@163.com>
2023-01-01 14:48:33 +0800

    feat: add global styles

--- React-Backstage-Manage-System/src/main.tsx
+++ React-Backstage-Manage-System/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'reset-css'
+import './assets/styles/global.scss'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
