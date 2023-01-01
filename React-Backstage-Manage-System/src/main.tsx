646cf885562d4620c7816295ee3c07be8700b7c2
skyclouds2001 <skyclouds2001@163.com>
2023-01-01 14:57:49 +0800

    build: add alias support

--- React-Backstage-Manage-System/src/main.tsx
+++ React-Backstage-Manage-System/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'reset-css'
-import './assets/styles/global.scss'
-import App from './App'
+import '@/assets/styles/global.scss'
+import App from '@/App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
