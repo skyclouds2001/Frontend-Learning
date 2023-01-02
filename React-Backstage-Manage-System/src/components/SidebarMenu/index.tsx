e0b1e10e98cb9377c450d366b626cc90e03b18a5
skyclouds2001 <skyclouds2001@163.com>
2023-01-02 19:23:36 +0800

    feat: add independent page

---
+++ React-Backstage-Manage-System/src/components/SidebarMenu/index.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Menu } from 'antd'
+import { DesktopOutlined, FileOutlined, PieChartOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons'
+import type { MenuProps } from 'antd'
+
+type MenuItem = Required<MenuProps>['items'][number]
+
+const items: MenuItem[] = [
+  {
+    key: '1',
+    label: 'Option 1',
+    icon: <PieChartOutlined />,
+  },
+  {
+    key: '2',
+    label: 'Option 2',
+    icon: <DesktopOutlined />,
+  },
+  {
+    key: 'sub1',
+    label: 'Option 3',
+    icon: <UserOutlined />,
+    children: [
+      {
+        key: '3',
+        label: 'Option 3-1',
+      },
+      {
+        key: '4',
+        label: 'Option 3-2',
+      },
+      {
+        key: '5',
+        label: 'Option 3-3',
+      },
+    ],
+  },
+  {
+    key: 'sub2',
+    label: 'Option 4',
+    icon: <TeamOutlined />,
+    children: [
+      {
+        key: '6',
+        label: 'Option 4-1',
+      },
+      {
+        key: '7',
+        label: 'Option 4-2',
+      },
+    ],
+  },
+  {
+    key: '9',
+    label: 'Option 5',
+    icon: <FileOutlined />,
+  },
+]
+
+const SidebarMenu: React.FC = () => {
+  const navigate = useNavigate()
+
+  const[openKeys, setOpenKeys] = useState<string[]>([])
+
+  return (
+    <Menu
+      theme="dark"
+      defaultSelectedKeys={ ['1'] }
+      mode="inline"
+      openKeys={ openKeys }
+      items={ items }
+      onClick={ e => navigate(e.key) }
+      onOpenChange={ e => setOpenKeys(e.filter(v => !openKeys.includes(v))) }
+    />
+  )
+}
+
+export default SidebarMenu
--- React-Backstage-Manage-System/src/views/Home.tsx
+++ React-Backstage-Manage-System/src/views/Home.tsx
@@ -1,69 +1,21 @@
 import React, { useState } from 'react'
-import { useNavigate, Outlet } from 'react-router-dom'
-import { Breadcrumb, Layout, Menu, theme } from 'antd'
-import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from '@ant-design/icons'
-import type { MenuProps } from 'antd'
+import { Outlet } from 'react-router-dom'
+import { Breadcrumb, Layout, theme } from 'antd'
+import SidebarMenu from '@/components/SidebarMenu'
 
 const { Header, Content, Footer, Sider } = Layout
 
-type MenuItem = Required<MenuProps>['items'][number]
-
-type MenuInfo = {
-  key: string
-  keyPath: string[]
-  /** @deprecated This will not support in future. You should avoid to use this */
-  item: React.ReactInstance
-  domEvent: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>
-}
-
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[],
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem
-}
-
-const items: MenuItem[] = [
-  getItem('Option 1', '1', <PieChartOutlined />),
-  getItem('Option 2', '2', <DesktopOutlined />),
-  getItem('User', 'sub1', <UserOutlined />, [
-    getItem('Tom', '3'),
-    getItem('Bill', '4'),
-    getItem('Alex', '5'),
-  ]),
-  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-  getItem('Files', '9', <FileOutlined />),
-]
-
 const Home: React.FC = () => {
-  const navigate = useNavigate()
   const [collapsed, setCollapsed] = useState(false)
   const {
     token: { colorBgContainer },
   } = theme.useToken()
 
-  const menuClick = (e: MenuInfo) => {
-    navigate(e.key)
-  }
-
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
         <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} onClick={menuClick} />
+        <SidebarMenu />
       </Sider>
       <Layout className="site-layout">
         <Header style={{ padding: 0, background: colorBgContainer }}>
