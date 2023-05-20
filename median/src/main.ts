be073b05833f9bd3c61a60ef73c820cf69594078
skyclouds2001 <skyclouds2001@163.com>
2023-05-20 19:15:02 +0800

    feat: use swagger

--- median/src/main.ts
+++ median/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  SwaggerModule.setup(
+    'api',
+    app,
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Median')
+        .setDescription('The Median API description')
+        .setVersion('0.1')
+        .build(),
+    ),
+  );
   await app.listen(3000);
 }
 bootstrap();
