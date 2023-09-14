110f109c4e0e2ad8230882c51367a272fffebd02
skyclouds2001 <skyclouds2001@163.com>
2023-09-14 22:16:44 +0800

    feat: update background-fetch

--- next-learning/background-fetch.js
+++ next-learning/background-fetch.js
@@ -2,34 +2,66 @@
 /// <reference lib="esnext" />
 /// <reference lib="webworker" />
 
-self.addEventListener("backgroundfetchclick", (e) => {
-  if (e.registration.result === "success") {
-    clients.openWindow("/play-movie");
-  } else {
-    clients.openWindow("/movie-download-progress");
-  }
-})
-
-self.addEventListener("backgroundfetchfail", (e) => {
-  e.updateUI({
-    title: "Download Fail",
-  })
-})
-
 self.addEventListener('backgroundfetchsuccess', (e) => {
-  e.waitUntil(() => {
-    return self.caches.open('movies').then((cache) => {
-      return e.registration.matchAll().then((records) => {
-        return Promise.all(records.map((record) => {
-          return record.responseReady.then((response) => {
-            cache.put(record.request, response)
-          })
-        }))
-      })
-    }).then(() => {
+  e.waitUntil(() => 
+    self.caches.open('movies').then((cache) =>
+      e.registration.matchAll().then((records) =>
+        Promise.all(
+          records.map((record) =>
+            record.responseReady.then((response) =>
+              cache.put(record.request, response)
+            )
+          )
+        )
+      )
+    ).then(() =>
       e.updateUI({
-        title: 'Move download complete',
+          title: 'Move download complete',
       })
-    })
-  })
+    )
+  )
+})
+
+self.addEventListener('backgroundfetchfail', (e) => {
+  e.waitUntil(() =>
+    self.caches.open('movies').then((cache) =>
+      e.registration.recordsAvailable && e.registration.matchAll().then((records) =>
+        Promise.all(
+          records.map((record) =>
+            record.responseReady.then((response) =>
+              cache.put(record.request, response)
+            )
+          )
+        )
+      )
+    ).then(() =>
+      e.updateUI({
+        title: 'Download Fail',
+      })
+    )
+  )
+})
+
+self.addEventListener('backgroundfetchabort', (e) => {
+  e.waitUntil(() =>
+    self.caches.open('movies').then((cache) =>
+      e.registration.recordsAvailable && e.registration.matchAll().then((records) =>
+        Promise.all(
+          records.map((record) =>
+            record.responseReady.then((response) =>
+              cache.put(record.request, response)
+            )
+          )
+        )
+      )
+    )
+  )
+})
+
+self.addEventListener('backgroundfetchclick', (e) => {
+  if (e.registration.result === 'success') {
+    self.clients.openWindow('/play-movie');
+  } else {
+    self.clients.openWindow('/movie-download-progress');
+  }
 })
