48b701432c2ca6d146b28766c63bc020cfdd8430
skyclouds2001 <skyclouds2001@163.com>
2023-09-22 15:46:46 +0800

    feat: update worker

--- next-learning/worker.js
+++ next-learning/worker.js
@@ -3,3 +3,6 @@ self.postMessage('message from worker')
 self.addEventListener('message', (e) => {
   console.log('receive message in worker: ', e.data)
 })
+
+const data = new ArrayBuffer(100)
+self.postMessage(data, [data])
